d706530374f2fed7172796f9e97859e6a7cc2a60
duan-duan <972065033@qq.com>
2017-06-05 15:57:25 +0800

    加密

---
+++ src/main/java/bullionClient/BullionClientHandler.java
@@ -0,0 +1,43 @@
+package bullionClient;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author: wangruirui
+ * @date: 2017/6/2
+ * @description:
+ */
+public class BullionClientHandler extends ChannelHandlerAdapter{
+
+    private int counter;
+    private byte[] req;
+
+    public BullionClientHandler(String msg){
+        req = (msg+"query time order "+ System.getProperty("line.separator")).getBytes();
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf message = null;
+        for (int i=0;i<100;i++){
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("now receive order :"+body+"; the counter is :"+ ++counter);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
---
+++ src/main/java/bullionClient/Encryption.java
@@ -0,0 +1,99 @@
+package bullionClient;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * @author: wangruirui
+ * @date: 2017/6/5
+ * @description:
+ */
+public class Encryption {
+
+   //java 加密代码  rsa
+    public static byte[] rsaEncryptGess (byte[] src,Key key) throws IOException,GeneralSecurityException{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,key);
+        ByteArrayOutputStream daos = new ByteArrayOutputStream();
+        for(int i=0;i<src.length;i=i+100){
+            int len = 0;
+            if((i+100)>src.length){
+                len = src.length-i;
+            }else{
+                len = 100;
+            }
+            byte[] tmp = new byte[len];
+            System.arraycopy(src,i,tmp,0,tmp.length);
+            daos.write(cipher.doFinal(tmp));
+        }
+        return daos.toByteArray();
+    }
+
+    //java 解密代码  rsa
+    public static byte[] rsaDescryptGess (byte[] src,Key key) throws IOException,GeneralSecurityException{
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,key);
+        ByteArrayOutputStream daos = new ByteArrayOutputStream();
+        for(int i = 0; i < src.length; i = i+128){
+            byte[] tmp = new byte[128];
+            System.arraycopy(src,i,tmp,0,tmp.length);
+            daos.write(cipher.doFinal(tmp));
+        }
+        return daos.toByteArray();
+    }
+
+
+
+
+    /**
+     * byte数组转为string
+     *
+     * @param encrytpByte
+     * @return
+     */
+    public static String bytesToString(byte[] encrytpByte) {
+        String result = "";
+        for (Byte bytes : encrytpByte) {
+            result += (char) bytes.intValue();
+        }
+        return result;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        try {
+
+            String encryptText = "12345678";
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+            keyPairGen.initialize(1024);
+            KeyPair keyPair = keyPairGen.generateKeyPair();
+            // Generate keys
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate(); // 私钥
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic(); // 公钥
+            byte[] e = rsaEncryptGess( encryptText.getBytes(),publicKey);
+            byte[] de = rsaDescryptGess(e,privateKey);
+            System.out.println(bytesToString(e));
+            System.out.println();
+
+            System.out.println(bytesToString(de));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
